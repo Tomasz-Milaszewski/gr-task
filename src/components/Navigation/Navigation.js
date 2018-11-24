8ffa38c921f8fe5ba2221d01943690bf2b51de5c
Tomek_M <tomasz.milaszewski@gmail.com>
2018-11-24 14:46:30 +0100

    adds navigation panel first layout

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,10 +1,20 @@
 import React, { Component } from "react";
 import "./Navigation.css";
-
 class Navigation extends Component {
     render() {
         return (
-          
+            <nav className="Navigation-container">
+                <div className="Navigation-content-container">
+                    <div className="Navigation-content-active">DASHBOARD<hr /></div>
+                    <div className="Navigation-content-passive">RECIPES<hr /></div>
+                    <div className="Navigation-content-passive">CHALLENGE<hr /></div>
+                </div>
+                <div className="Navigation-user-container">
+                    <div className="Navigation-user-image"></div>
+                    <div className="Navigation-user-name">Olivia Wilde</div>
+                    <div className="Navigation-user-chevron"></div>
+                </div>
+            </nav>
         )
     }
 }
