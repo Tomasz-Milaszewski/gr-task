4ef94463cb7b4156c5a55b2b12aeb419a166e0a5
Tomek_M <tomasz.milaszewski@gmail.com>
2018-11-24 18:52:54 +0100

    WIP: Hero and HeroWeeks components

--- src/components/HeroWeeks/HeroWeeks.js
+++ src/components/HeroWeeks/HeroWeeks.js
@@ -13,20 +13,62 @@ class HeroWeeks extends Component {
 
   render() {
     return (
-      <nav className="Navigation-container">
-        <div className="Navigation-content-container">
-          <div className={this.state.activeViewName === "DASHBOARD" ? "Navigation-content-active" : "Navigation-content-passive"}><a href="" onClick={this.handleClick}>DASHBOARD<hr /></a></div>
-          <div className={this.state.activeViewName === "RECIPES" ? "Navigation-content-active" : "Navigation-content-passive"}><a href="" onClick={this.handleClick}>RECIPES<hr /></a></div>
-          <div className={this.state.activeViewName === "CHALLENGE" ? "Navigation-content-active" : "Navigation-content-passive"}><a href="" onClick={this.handleClick}>CHALLENGE<hr /></a></div>
+      <div class="HeroWeeks-container">
+        
+        <div class="HeroWeeks-progress-container">
+          <p>	YOUR 12 WEEK PROGRESS</p>
+          <div class="HeroWeeks-bullets-container">
+            <span></span> 
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+            <span></span>
+          </div>
+          <div class="HeroWeeks-week-numbers-container">
+            <span>1</span>
+            <span>2</span>
+            <span>3</span>
+            <span>4</span>
+            <span>5</span>
+            <span>6</span>
+            <span>7</span>
+            <span>8</span>
+            <span>9</span>
+            <span>10</span>
+            <span>11</span>
+            <span>12</span>
+          </div>
         </div>
-        <div className="Navigation-user-container">
-          <div className="Navigation-user-image"></div>
-          <div className="Navigation-user-name">Olivia Wilde</div>
-          <div className="Navigation-user-chevron"></div>
+        
+        <div class="HeroWeeks-slider-container">
+          <button class="HeroWeeks-slider-arrow-left"></button>
+          <p class="HeroWeeks-slider-current-week">{`Week ${this.state.currentWeek}`}</p>
+          <button class="HeroWeeks-slider-arrow-right"></button>
         </div>
-      </nav>
+      </div>
     )
   }
 }
 
 export default HeroWeeks
+
+// {'&#8226'} full bullet
+// {'&#9702'} empty bullet
