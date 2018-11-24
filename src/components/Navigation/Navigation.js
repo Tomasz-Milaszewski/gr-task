52d1471f34bbd6a4c96b1f9f0e19d6e371f7e35a
Tomek_M <tomasz.milaszewski@gmail.com>
2018-11-24 16:44:00 +0100

    adds mock view changes on clicking at navigation links with style changes

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,24 @@
 import React, { Component } from "react";
 import "./Navigation.css";
 class Navigation extends Component {
+
+    state = {
+        activeViewName: "DASHBOARD"
+    }
+
+    handleClick = (event) => {
+        event.preventDefault();
+        if (event.target.className === "Navigation-content-active") {return; }
+        this.setState({activeViewName: event.target.innerText})
+    }
+
     render() {
         return (
             <nav className="Navigation-container">
                 <div className="Navigation-content-container">
-                    <div className="Navigation-content-active"><a href="">DASHBOARD<hr /></a></div>
-                    <div className="Navigation-content-passive"><a href="">RECIPES<hr /></a></div>
-                    <div className="Navigation-content-passive"><a href="">CHALLENGE<hr /></a></div>
+                    <div className={this.state.activeViewName === "DASHBOARD" ? "Navigation-content-active" : "Navigation-content-passive"}><a href="" onClick={this.handleClick}>DASHBOARD<hr /></a></div>
+                    <div className={this.state.activeViewName === "RECIPES" ? "Navigation-content-active" : "Navigation-content-passive"}><a href="" onClick={this.handleClick}>RECIPES<hr /></a></div>
+                    <div className={this.state.activeViewName === "CHALLENGE" ? "Navigation-content-active" : "Navigation-content-passive"}><a href="" onClick={this.handleClick}>CHALLENGE<hr /></a></div>
                 </div>
                 <div className="Navigation-user-container">
                     <div className="Navigation-user-image"></div>
