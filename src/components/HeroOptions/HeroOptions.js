a0756ed14c3ff285da5b079d9b5e7e5cca4e50eb
Tomek_M <tomasz.milaszewski@gmail.com>
2018-11-25 13:10:17 +0100

    adds and styles HeroOptions component

--- src/components/HeroOptions/HeroOptions.js
+++ src/components/HeroOptions/HeroOptions.js
@@ -2,7 +2,14 @@ import React, { Component } from "react";
 import "./HeroOptions.css";
 import broccoli_active from '../../images/herooptions-broccoli-active.png'
 import broccoli_nonactive from '../../images/herooptions-broccoli-nonactive.png'
-
+import cheese_active from '../../images/herooptions-cheese-active.png'
+import cheese_nonactive from '../../images/herooptions-cheese-nonactive.png'
+import meat_active from '../../images/herooptions-meat-active.png'
+import meat_nonactive from '../../images/herooptions-meat-nonactive.png'
+import fish_active from '../../images/herooptions-fish-active.png'
+import fish_nonactive from '../../images/herooptions-fish-nonactive.png'
+import chicken_active from '../../images/herooptions-chicken-active.png'
+import chicken_nonactive from '../../images/herooptions-chicken-nonactive.png'
 class HeroOptions extends Component {
 
   render() {
@@ -11,6 +18,10 @@ class HeroOptions extends Component {
         <p>SELECT YOUR PROTEIN OPTIONS</p>
         <div className="HeroOptions-icons-container">
           <img src={broccoli_active} alt="Broccoli" />
+          <img src={cheese_active} alt="Cheese" />
+          <img src={meat_active} alt="Meat" />
+          <img src={fish_active} alt="Fish" />
+          <img src={chicken_active} alt="Chicken" />
         </div>
       </div>
     )
