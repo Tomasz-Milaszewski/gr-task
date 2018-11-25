054fe1875829ad8c2ea6999e1fe4b7c5463ec983
Tomek_M <tomasz.milaszewski@gmail.com>
2018-11-25 23:04:51 +0100

    WIP: HeroDaysOn component

---
+++ src/components/HeroDaysOn/HeroDaysOn.js
@@ -0,0 +1,24 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import "./HeroDaysOn.css";
+import tick from '../../images/herodayson-tick.png'
+class HeroDaysOn extends Component {
+
+  static propTypes = {
+    day: PropTypes.object.isRequired
+  }
+
+  render() {
+    return (
+      <div className="HeroDaysOn-container">
+        <div className="HeroDaysOn-day-number">DAY{this.props.day.dayNumber}</div>
+        <div className={this.props.day.sixAMShake === true ? "HeroDaysOn-meal HeroDaysOn-bode-shake" : "HeroDaysOn-meal"}>
+          <p className="HeroDaysOn-meal-name">{this.props.day.sixAMMeal}</p>
+          {this.props.day.sixAMTick === true && <img src={tick} alt="" className="HeroDaysOn-tick"></img>}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default HeroDaysOn
