c6461c49b82779fcc2d69f7831b78dbe3a5cfc9e
Tomek_M <tomasz.milaszewski@gmail.com>
2018-11-26 13:14:20 +0100

    WIP:styles days components but for ticks

--- src/components/HeroDaysOn/HeroDaysOn.js
+++ src/components/HeroDaysOn/HeroDaysOn.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import "./HeroDaysOn.css";
 import tick from '../../images/herodayson-tick.png'
+import workout_done from '../../images/herodayson-workout-done.png'
+import workout_notdone from '../../images/herodayson-workout-notdone.png'
 class HeroDaysOn extends Component {
 
   static propTypes = {
@@ -16,13 +18,15 @@ class HeroDaysOn extends Component {
       <div className="HeroDaysOn-container">
         {days.map((day) =>
           <div className="HeroDaysOn-single-day">
-            <div className="HeroDaysOn-day-number">DAY{day.dayNumber}</div>
+            <div className="HeroDaysOn-day-number">DAY {day.dayNumber}</div>
             {hours.map((hour) =>
               <div className={day[`${hour}Shake`] === true ? "HeroDaysOn-meal-cont HeroDaysOn-bode-shake" : "HeroDaysOn-meal-cont"}>
                 <p className="HeroDaysOn-meal-name">{day[`${hour}Meal`]}</p>
                 {day[`${hour}Tick`] === true && <img src={tick} alt="" className="HeroDaysOn-tick"></img>}
               </div>
             )}
+            <div className="HeroDaysOn-bottom">{day.lowCarb === true ? 'LOW-CARB' : 'HIGH-CARB'}</div>
+            <div className="HeroDaysOn-workout"> {day.workoutDone === true ? <img src={workout_done} alt=""></img> : <img src={workout_notdone} alt=""></img>}</div>
           </div>
         )}
       </div>
